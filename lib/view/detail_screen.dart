3b95bf9392ddc915e1ca302b6b14d5ed21414496
Rohan <112865673+RohanParmar1134@users.noreply.github.com>
2024-07-27 14:57:08 +0530

    dropdown issue solved

--- lib/view/detail_screen.dart
+++ lib/view/detail_screen.dart
@@ -22,11 +22,14 @@ class _DetailScreenState extends State<DetailScreen> {
 
   @override
   void initState() {
+    Future.microtask(()async{
+      _dropdownItems1 = scanBagViewModal.destinationList.map((item) => item['destination'] as String).toList();
+      _dropdownItems2 = scanBagViewModal.workOrderList.map((item) => item['workOrder'] as String).toList();
+      setState(() {});
+    });
+    
     super.initState();
-
-
-    _dropdownItems1 = scanBagViewModal.destinationList.map((item) => item['destination'] as String).toList();
-    _dropdownItems2 = scanBagViewModal.workOrderList.map((item) => item['workOrder'] as String).toList();
+   
   }
 
   @override
@@ -48,57 +51,59 @@ class _DetailScreenState extends State<DetailScreen> {
               Text("Source Of Material : " + widget.sourceOfMaterial??""),
               Text("Weight : " +widget.weight??""),
 
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8, // 80% of the screen width
-                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(color: Colors.grey),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 1.0,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child:
-                  _buildDropdown(
-                    context,
-                    "Select Destination",
-                    _selectedItem1,
-                    _dropdownItems1,
-                        (String? newValue) {
-                      setState(() {
-                        _selectedItem1 = newValue;
-                      });
-                    },
-                  ),
-                  // DropdownButtonHideUnderline(
-                  //   child: DropdownButton<String>(
-                  //     hint: Text("Select Item"),
-                  //     value: _selectedItem1,
-                  //     onChanged: (String? newValue) {
-                  //       setState(() {
-                  //         _selectedItem1 = newValue!;
-                  //       });
-                  //     },
-                  //     dropdownColor: Colors.white,
-                  //       // .toList()
-                  //     items: _dropdownItems1.map((item) => item['destination']!).toList()<DropdownMenuItem<String>>((String value) {
-                  //       return DropdownMenuItem<String>(
-                  //         value: value,
-                  //         child: Text(value),
-                  //       );
-                  //     }).toList(),
-                  //     isExpanded: true,
-                  //   ),
-                  // ),
-                ),
+              _buildDropdown(
+                context,
+                "Select Destination",
+                _selectedItem1,
+                _dropdownItems1,
+                    (String? newValue) {
+                  setState(() {
+                    _selectedItem1 = newValue;
+                  });
+                },
               ),
+
+              // Padding(
+              //   padding: const EdgeInsets.only(top: 8.0),
+              //   child: Container(
+              //     width: MediaQuery.of(context).size.width * 0.8, // 80% of the screen width
+              //     padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+              //     decoration: BoxDecoration(
+              //       color: Colors.white,
+              //       borderRadius: BorderRadius.circular(10.0),
+              //       border: Border.all(color: Colors.grey),
+              //       boxShadow: [
+              //         BoxShadow(
+              //           color: Colors.black12,
+              //           blurRadius: 1.0,
+              //           offset: Offset(0, 2),
+              //         ),
+              //       ],
+              //     ),
+              //     child:
+              //
+              //     // DropdownButtonHideUnderline(
+              //     //   child: DropdownButton<String>(
+              //     //     hint: Text("Select Item"),
+              //     //     value: _selectedItem1,
+              //     //     onChanged: (String? newValue) {
+              //     //       setState(() {
+              //     //         _selectedItem1 = newValue!;
+              //     //       });
+              //     //     },
+              //     //     dropdownColor: Colors.white,
+              //     //       // .toList()
+              //     //     items: _dropdownItems1.map((item) => item['destination']!).toList()<DropdownMenuItem<String>>((String value) {
+              //     //       return DropdownMenuItem<String>(
+              //     //         value: value,
+              //     //         child: Text(value),
+              //     //       );
+              //     //     }).toList(),
+              //     //     isExpanded: true,
+              //     //   ),
+              //     // ),
+              //   ),
+              // ),
               Padding(
                 padding: const EdgeInsets.only(top: 8.0),
                 child: Container(
